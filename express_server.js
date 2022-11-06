bba199af1d949fa1dae02bf64c6b593b60d0d9e7
Fayez Alsaadi (He/Him) <fayez@legible.com>
2022-11-06 19:04:57 +0400

    Display a single URL

--- express_server.js
+++ express_server.js
@@ -22,8 +22,15 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase };
+    const templateVars = {urls: urlDatabase};
     res.render("urls_index", templateVars);
+});
+
+
+// display a single URL
+app.get("/urls/:id", (req, res) => {
+    const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
+    res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
